2d51cbb4ca427c48c9a27bcab7e8530e594b1ec0
mahfuj10 <mahfujglobal@gmail.com>
2024-04-08 01:53:07 +0600

    save

--- dist/routes/email.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var express = require('express');
-var emailController = require('../controllers/email.controller');
-var router = express.Router();
-router.post('/send', emailController.sendEmail);
-module.exports = router;
